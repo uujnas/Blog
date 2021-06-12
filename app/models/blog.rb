6bd11d65c72dbe605ed05f29a431f97faa942c5a
uujnas <sanjubasnet036@gmail.com>
2021-06-12 20:17:47 +0545

    Bug fixes

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,7 @@
 class Blog < ApplicationRecord
     has_one_attached :profile_pic
+    # validates :name, presence: true
+    validates :title, :written_by, :job_title,:description, presence: true
+    validates :title, :length =>{:minimum => 10}
+    validates :description, :length =>{:minimum =>50}
 end
