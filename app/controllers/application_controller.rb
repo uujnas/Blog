917fe7a77ed5d46340c1b7602c669406c0d09ed3
uujnas <sanjubasnet036@gmail.com>
2021-06-19 12:31:59 +0545

    created user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys:[:username, :first_name, :last_name])
+        devise_parameter_sanitizer.permit(:account_update,keys: [:username, :first_name, :last_name])
         # devise_parameter_sanitizer.for(:sign_up){ |u| u.permit(:first_name,:last_name,:username,:email, :password, :remember_me)}
         # devise_parameter_sanitizer.for(:sign_in){ |u| u.permit(:email, :password)}
         # devise_parameter_sanitizer.for(:account_update){ |u| u.permit(:first_name,:last_name,:username,:email, :password, :remember_me)}
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'blogs#index'
   resources :blogs
   resources :categories
   resources :users
+  # get 'users/:id', to:'users#show', as: :user
 end
